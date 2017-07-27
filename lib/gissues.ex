5d8e0528dca27d43565db2fd5bdc97012cd11ba4
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-07-27 17:58:24 +0200

    init

---
+++ lib/gissues.ex
@@ -0,0 +1,18 @@
+defmodule Gissues do
+  @moduledoc """
+  Documentation for Gissues.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gissues.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gissues_test.exs
@@ -0,0 +1,8 @@
+defmodule GissuesTest do
+  use ExUnit.Case
+  doctest Gissues
+
+  test "greets the world" do
+    assert Gissues.hello() == :world
+  end
+end
